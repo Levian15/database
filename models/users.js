0aac46dcd46239ffa74e2a822a0f6a371cc11aef
Ian Levi Lagunes Gallegos <ilgallegos.f@gmail.com>
2023-10-12 14:04:01 -0600

    Query Params

--- models/users.js
+++ models/users.js
@@ -13,7 +13,22 @@ const usersModel ={
 WHERE
 
     id=?   
-    `
-}
+    `,
+    addRow:`
+    INSERT INTO
+    Users(
+        username,
+        email,
+        password,
+        name,
+        lastname,
+        phone_number,
+        role_id,
+        is_active
+    )
+    VALUES(
+      ?,?,?,?,?,?,?,?  
+    ) `,
+};
 
 module.exports=usersModel;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const{Router}=require('express');
-const{usersList,listUserByID}=require('../controllers/users');
+const{usersList,listUserByID,addUser}=require('../controllers/users');
 
 const router = Router();
 
 //http://localhost:3000/api/v1/users/
 router.get('/',usersList);
 router.get('/:id',listUserByID,);
+router.put('/',addUser);
 //router.post('/',usersList);
 //router.put('/',usersList);
 //router.patch('/',usersList);
